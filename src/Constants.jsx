b54960e7f904fd2b523fdc9b978636a208cb949c
Flavio.Cervantes <fcervantes@csumb.edu>
2026-07-22 00:58:47 -0700

    Vanilla image

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,6 +1,7 @@
 
 export const REGISTRAR_URL = 'http://localhost:8080';
 export const GRADEBOOK_URL = 'http://localhost:8081';
+// export const SERVER_URL = 'http://
 
 export const userTypes = ['STUDENT', 'ADMIN', 'INSTRUCTOR'];
 export const semesters = ['Spring', 'Fall'];
