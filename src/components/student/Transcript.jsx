075a3be0e79a092b243c034b482546cd26203dbf
Monica Um <mum@csumb.edu>
2026-07-22 18:42:02 -0700

    added information for retrieving transcript table. Allowed feature of filtering by term. not tested yet and may have errors.

--- src/components/student/Transcript.jsx
+++ src/components/student/Transcript.jsx
@@ -9,6 +9,21 @@ const Transcript = () => {
   const [courses, setCourses] = useState([]);
 
   const fetchData = async () => {
+
+    // allows user to filter by term
+    const {year, semester} = term || {};
+    const params = new URLSearchParams();
+    
+    if (year) {
+      params.append('year', year);
+    }
+
+    if (semester) {
+      params.append('semester', semester);
+    }
+    
+    const query = params.toString();
+
     try {
       const response = await fetch(`${REGISTRAR_URL}/transcripts`,
         {
@@ -23,11 +38,16 @@ const Transcript = () => {
       if (response.ok) {
         const data = await response.json();
         setCourses(data);
-      } else {
+
+        // this clears error message after succesful fetch
+        setMessage('');
+      } 
+      else {
         const body = await response.json();
         setMessage(body);
       }
-    } catch (err) {
+    } 
+    catch (err) {
       setMessage(err);
     }
   }
@@ -41,9 +61,36 @@ const Transcript = () => {
   return (
     <>
       <h3>Transcript</h3>
-      <p>To be implemented.  Display a table showing the course a student has taken.
-        The table columns are given in headers.
-      </p>
+      <Messages response = {message}/>
+
+      <SelectTerm buttonTest = "Filter by Term" onClick = {fetchData}/>
+      
+      {/*Prevents fetching error if no term entered for filtering*/}
+      <button onClick = {() => fetchData()}>Show ALL Terms</button>
+
+      <table className = "Center">
+        <thead>
+          <tr>
+            {headers.map((h, idx) => (
+              <th key = {idx}>{h}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {courses.map((c) => (
+            <tr key = {c.enrollmentId}>
+              <td>{c.year}</td>
+              <td>{c.semester}</td>
+              <td>{c.courseId}</td>
+              <td>{c.sectionId}</td>
+              <td>{c.title}</td>
+              <td>{c.credits}</td>
+              <td>{c.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
     </>
   );
